e7918ac3a5ca027decaae3d63694cfe97f4789cf
snakepatch53 <snakepatch53@gmail.com>
2022-08-18 01:54:53 -0500

    xd

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 $proyect = array(
     "name" => "/repositorio_moronanet",
-    "root_absolute" => "http://localhost/repositorio_moronanet/"
+    "root_absolute" => "http://localhost/repositorio_moronanet/   cambio"
 );
